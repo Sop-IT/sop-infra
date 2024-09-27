2016ec27d09bf8bcbb330ec514319770d454a50c
Leorevoir <leoquinzler@epitech.eu>
2024-09-27 12:08:58 +0200

    update plugin version to 1.0.4

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='sop-infra',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'sop-utils>=1.0.7',
-    ],
     description = "Manage infrastructure informations of each site.",
     author="Leorevoir",
     author_email="leo.quinzler@epitech.eu",
