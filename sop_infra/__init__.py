b466dd9e1e87ae409f180e43c20efff5efe0862e
Leorevoir <leo.quinzler@epitech.eu>
2024-11-14 16:21:06 +0100

    add: v.0.1.2

--- sop_infra/__init__.py
+++ sop_infra/__init__.py
@@ -5,7 +5,7 @@ class SopInfraConfig(PluginConfig):
     name = "sop_infra"
     verbose_name = "SOP Infra"
     description = "Manage infrastructure informations of each site"
-    version = "0.1.1"
+    version = "0.1.2"
     author = "Leorevoir"
     author_email = "leo.quinzler@epitech.eu"
     base_url = "sop-infra"
