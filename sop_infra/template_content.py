5c516398da4fa6b7ec56f802d6065f4dea5f9337
Leorevoir <leo.quinzler@epitech.eu>
2024-11-19 09:28:00 +0100

    feat: new template_content class parse -> README

--- sop_infra/template_content.py
+++ sop_infra/template_content.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.conf import settings
 
 from netbox.plugins import PluginTemplateExtension
@@ -6,6 +8,7 @@ from sop_infra.models import SopInfra
 
 
 ALLOWED_PANELS = ['meraki', 'classification', 'sizing']
+ALLOWED_POSITIONS = ['left_page', 'right_page']
 
 
 def create_new_panel(self):
@@ -28,21 +31,7 @@ class SopInfraDashboard:
 
     def __init__(self):
         self.settings = settings.PLUGINS_CONFIG.get('sop_infra', {})
-        self.extensions = self.get_dashboard_config()
-
-
-    def get_display_position(self, panel):
-
-        position = self.settings.get('display_default')
-        custom = self.settings.get('display_custom')
-
-        if custom is None:
-            return position
-
-        if exists := custom.get(panel):
-            return custom.get(panel)
-
-        return position
+        self.extensions = self.get_display_extensions()
 
 
     def get_html_panel(self, panel):
@@ -50,36 +39,63 @@ class SopInfraDashboard:
         return self.template_name.format(panel)
 
 
-    def get_dashboard_config(self):
+    def get_display_position(self, panel, display):
+
+        if exists := display.get(panel):
+
+            if exists not in ALLOWED_POSITIONS:
+                return None
+
+            return exists
+
+        return None
+
+
+    def get_display_extensions(self):
 
         extensions = []
-        panels = self.settings.get('panels')
+        _display = self.settings.get('display')
 
-        if panels is None:
-            return None
+        # no configuration
+        if _display is None:
+            return
 
-        for panel in panels:
+        # error handling
+        if not isinstance(_display, dict):
+            logging.error(f'Invalid syntax "{_display}", must be a dict.')
+            return
 
-            if panel not in ['meraki', 'classification', 'sizing']:
-                print('WARNING:  only select panels between', ALLOWED_PANELS)
+        for panel in _display:
+
+            if panel not in ALLOWED_PANELS:
+                logging.error(f'Invalid panel "{panel}", valid display are:', ALLOWED_PANELS)
                 continue
 
-            display = self.get_display_position(panel)
+            # return the position of {panel:position}
+            position = self.get_display_position(panel, _display)
+
+            if position is None:
+                logging.error(f'Invalid position "{position}", valid positions are:', ALLOWED_POSITIONS)
+                continue
+
+            # creates dynamically a template extension class
             new_class = type(
-                f'{panel}_SopInfra_dash_extension',
+                f'{panel}_SopInfra_panel_extension',
                 (PluginTemplateExtension,), {
                     'model': self.model,
                     'what': self.get_html_panel(panel),
-                    display: create_new_panel
+                    position: create_new_panel
                 }
             )
             extensions.append(new_class)
+
 
         return extensions
 
 
     def push(self):
         return self.extensions
+
 
 
 template_extensions = SopInfraDashboard().push()
