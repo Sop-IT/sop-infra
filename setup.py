8b34235f044f9fca7f091388d72eb49b490e31e2
Leorevoir <leo.quinzler@epitech.eu>
2024-11-19 09:28:23 +0100

    add: v0.1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sop-infra',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(),
     include_package_data=True,
     description = "Manage infrastructure informations of each site.",
