f474c4188b59908b2e9a7e808890d3db09d6e72f
Leorevoir <leo.quinzler@epitech.eu>
2024-11-25 13:18:16 +0100

    add: related models in templates / views

--- sop_infra/views/prisma.py
+++ sop_infra/views/prisma.py
@@ -3,44 +3,53 @@ from django.shortcuts import redirect
 
 from netbox.views import generic
 from utilities.permissions import get_permission_for_model
-from utilities.views import GetRelatedModelsMixin, ObjectPermissionRequiredMixin 
+from utilities.views import GetRelatedModelsMixin, ObjectPermissionRequiredMixin
 
 from sop_infra.filtersets import (
     PrismaEndpointFilterset,
     PrismaAccessLocationFilterset,
-    PrismaComputedAccessLocationFilterset
+    PrismaComputedAccessLocationFilterset,
 )
 from sop_infra.models import (
-    PrismaEndpoint, PrismaAccessLocation, PrismaComputedAccessLocation
+    SopInfra,
+    PrismaEndpoint,
+    PrismaAccessLocation,
+    PrismaComputedAccessLocation,
 )
 from sop_infra.forms import (
-    PrismaEndpointForm, PrismaAccessLocationForm, PrismaComputedAccessLocationForm,
-    PrismaEndpointFilterForm, PrismaAccessLocationFilterForm, PrismaComputedAccessLocationFilterForm
+    PrismaEndpointForm,
+    PrismaAccessLocationForm,
+    PrismaComputedAccessLocationForm,
+    PrismaEndpointFilterForm,
+    PrismaAccessLocationFilterForm,
+    PrismaComputedAccessLocationFilterForm,
 )
 from sop_infra.tables import (
-    PrismaEndpointTable, PrismaAccessLocationTable, PrismaComputedAccessLocationTable
+    PrismaEndpointTable,
+    PrismaAccessLocationTable,
+    PrismaComputedAccessLocationTable,
 )
 from sop_infra.utils import PrismaAccessLocationRecomputeMixin
 
 
 __all__ = (
-    'PrismaEndpointEditView',
-    'PrismaEndpointListView',
-    'PrismaEndpointDeleteView',
-    'PrismaEndpointDetailView',
-    'PrismaAccessLocationEditView',
-    'PrismaAccessLocationListView',
-    'PrismaAccessLocationDeleteView',
-    'PrismaAccessLocationDetailView',
-    'PrismaAccessLocationRefreshView',
-    'PrismaComputedAccessLocationEditView',
-    'PrismaComputedAccessLocationListView',
-    'PrismaComputedAccessLocationDeleteView',
-    'PrismaComputedAccessLocationDetailView',
+    "PrismaEndpointEditView",
+    "PrismaEndpointListView",
+    "PrismaEndpointDeleteView",
+    "PrismaEndpointDetailView",
+    "PrismaAccessLocationEditView",
+    "PrismaAccessLocationListView",
+    "PrismaAccessLocationDeleteView",
+    "PrismaAccessLocationDetailView",
+    "PrismaAccessLocationRefreshView",
+    "PrismaComputedAccessLocationEditView",
+    "PrismaComputedAccessLocationListView",
+    "PrismaComputedAccessLocationDeleteView",
+    "PrismaComputedAccessLocationDetailView",
 )
 
 
-#______________
+# ______________
 # Endpoint
 
 
@@ -50,11 +59,9 @@ class PrismaEndpointEditView(generic.ObjectEditView):
     form = PrismaEndpointForm
 
 
-
 class PrismaEndpointDeleteView(generic.ObjectDeleteView):
 
     queryset = PrismaEndpoint.objects.all()
-
 
 
 class PrismaEndpointListView(generic.ObjectListView):
@@ -65,13 +72,12 @@ class PrismaEndpointListView(generic.ObjectListView):
     filterset_form = PrismaEndpointFilterForm
 
 
-
 class PrismaEndpointDetailView(generic.ObjectView):
 
     queryset = PrismaEndpoint.objects.all()
 
 
-#______________
+# ______________
 # AccessLocation
 
 
@@ -81,66 +87,62 @@ class PrismaAccessLocationEditView(generic.ObjectEditView):
     form = PrismaAccessLocationForm
 
 
-
 class PrismaAccessLocationDeleteView(generic.ObjectDeleteView):
 
     queryset = PrismaAccessLocation.objects.all()
 
 
-
 class PrismaAccessLocationListView(generic.ObjectListView):
 
-    template_name:str = "sop_infra/tools/tables.html"
+    template_name: str = "sop_infra/tools/tables.html"
     queryset = PrismaAccessLocation.objects.all()
     table = PrismaAccessLocationTable
     filterset = PrismaAccessLocationFilterset
     filterset_form = PrismaAccessLocationFilterForm
 
     def get_extra_context(self, request) -> dict:
-        '''add title context for recompute button in template'''
+        """add title context for recompute button in template"""
         context = super().get_extra_context(request)
-        context['title'] = "PRISMA Access Locations"
+        context["title"] = "PRISMA Access Locations"
         return context
 
 
-
-class PrismaAccessLocationDetailView(
-    generic.ObjectView, GetRelatedModelsMixin):
+class PrismaAccessLocationDetailView(generic.ObjectView, GetRelatedModelsMixin):
 
     queryset = PrismaAccessLocation.objects.all()
 
     def get_extra_context(self, request, instance) -> dict:
-        '''
+        """
         additional context for related models/objects
-        '''
+        """
         related_models = self.get_related_models(
             request,
             instance,
             # later SopInfra
         )
-        return {'related_models':related_models}
-
+        return {"related_models": related_models}
 
 
 class PrismaAccessLocationRefreshView(
-    View,
-    PrismaAccessLocationRecomputeMixin,
-    ObjectPermissionRequiredMixin):
+    View, PrismaAccessLocationRecomputeMixin, ObjectPermissionRequiredMixin
+):
 
     model = PrismaAccessLocation
-    return_url = '/plugins/sop-infra/access_location/'
+    return_url = "/plugins/sop-infra/access_location/"
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
 
         # if not perm to change object, raise no permissions
-        if not request.user.has_perm(get_permission_for_model(PrismaAccessLocation, 'view')):
+        if not request.user.has_perm(
+            get_permission_for_model(PrismaAccessLocation, "view")
+        ):
             return self.handle_no_permission()
 
         self.try_recompute_access_location()
         return redirect(self.return_url)
 
 
-#______________
+# ______________
 # ComputedAccessLocation
 
 
@@ -150,11 +152,9 @@ class PrismaComputedAccessLocationEditView(generic.ObjectEditView):
     form = PrismaComputedAccessLocationForm
 
 
-
 class PrismaComputedAccessLocationDeleteView(generic.ObjectDeleteView):
 
     queryset = PrismaComputedAccessLocation.objects.all()
-
 
 
 class PrismaComputedAccessLocationListView(generic.ObjectListView):
@@ -165,20 +165,22 @@ class PrismaComputedAccessLocationListView(generic.ObjectListView):
     filterset_form = PrismaComputedAccessLocationFilterForm
 
 
-
-class PrismaComputedAccessLocationDetailView(
-    generic.ObjectView, GetRelatedModelsMixin):
+class PrismaComputedAccessLocationDetailView(generic.ObjectView, GetRelatedModelsMixin):
 
     queryset = PrismaComputedAccessLocation.objects.all()
 
     def get_extra_context(self, request, instance) -> dict:
-        '''
+        """
         additional context for related models/objects
-        '''
-        related_models = self.get_related_models(
-            request,
-            instance,
-            # later SopInfra
-        )
-        return {'related_models':related_models}
+        """
+        access = PrismaAccessLocation.objects.filter(compute_location=instance)
+        endpoint = PrismaEndpoint.objects.filter(
+            access_location__in=access
+        ).values_list("id", flat=True)
+        extra = [(SopInfra.objects.all(), {"endpoint__in": endpoint})]
 
+        related_models = self.get_related_models(request, instance, extra=extra)
+        return {
+            "related_models": related_models,
+            "access_location": PrismaAccessLocation,
+        }
