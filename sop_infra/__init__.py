f87bdf7f3463fc9ba59941df0276f96e5c63aefa
Leorevoir <leo.quinzler@epitech.eu>
2024-11-25 15:36:51 +0100

    fix: templates prisma access location GPS display

--- sop_infra/__init__.py
+++ sop_infra/__init__.py
@@ -5,7 +5,7 @@ class SopInfraConfig(PluginConfig):
     name = "sop_infra"
     verbose_name = "SOP Infra"
     description = "Manage infrastructure informations of each site"
-    version = "0.4.6"
+    version = "0.4.7"
     author = "Leorevoir"
     author_email = "leo.quinzler@epitech.eu"
     base_url = "sop-infra"
