d07458c901f725df30bcda728e36c05ac322321b
tibobo67 <48986982+tibobo67@users.noreply.github.com>
2025-05-27 10:01:00 +0200

    v0.4.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sop-infra",
-    version="0.4.11",
+    version="0.4.12",
     packages=find_packages(),
     include_package_data=True,
     description="Manage infrastructure informations of each site.",
