223c30110f67403390c8f85c0c21734ac89137f1
tibobo67 <48986982+tibobo67@users.noreply.github.com>
2025-06-05 13:26:24 +0200

    correctif construction des menus

--- sop_infra/navigation.py
+++ sop_infra/navigation.py
@@ -1,22 +1,71 @@
 from django.utils.translation import gettext_lazy as _
 
-from netbox.navigation import *
-from netbox.navigation.menu import MENUS
+from netbox.plugins import PluginMenu, PluginMenuButton, PluginMenuItem
 
 
-INFRA = Menu(
+menu = PluginMenu(
     label=_("Infrastructure"),
     icon_class="mdi mdi-router-network-wireless",
     groups=(
-        MenuGroup(
-            label=_("Infrastructure"),
-            items=(
-                MenuItem(
+        (
+            "Meraki",
+            (
+                PluginMenuItem(
+                    link="plugins:sop_infra:sopmerakidash_list",
+                    link_text="Dashboards",
+                    #permissions=["sop_infra.view_merakidashs"],
+                    buttons=(
+                        PluginMenuButton(
+                            link=f"plugins:sop_infra:sopmerakidash_refresh",
+                            title="Recompute",
+                            icon_class="mdi mdi-refresh",
+                            #permissions=[f"sop_infra.change_sop_infra"],
+                        ),
+                        PluginMenuButton(
+                            link="plugins:sop_infra:sopmerakidash_add",
+                            title="Add",
+                            icon_class="mdi mdi-plus-thick",
+                            #permissions=["sop_infra.add_merakidashs"],
+                        ),
+                    ),
+                ),
+                PluginMenuItem(
+                    link="plugins:sop_infra:sopmerakiorg_list",
+                    link_text="Organizations",
+                    #permissions=["sop_infra.view_merakiorgs"],
+                    buttons=(
+                        PluginMenuButton(
+                            link="plugins:sop_infra:sopmerakiorg_add",
+                            title="Add",
+                            icon_class="mdi mdi-plus-thick",
+                            #permissions=["sop_infra.add_merakiorgs"],
+                        ),
+                    ),
+                ),
+                PluginMenuItem(
+                    link="plugins:sop_infra:sopmerakinet_list",
+                    link_text="Networks",
+                    #permissions=["sop_infra.view_merakinets"],
+                    buttons=(
+                        PluginMenuButton(
+                            link="plugins:sop_infra:sopmerakinet_add",
+                            title="Add",
+                            icon_class="mdi mdi-plus-thick",
+                            #permissions=["sop_infra.add_merakinets"],
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        (
+            _("Infrastructure"),
+            (
+                PluginMenuItem(
                     link=f"plugins:sop_infra:class_list",
                     link_text=_("Classifications"),
                     permissions=[f"sop_infra.view_sopinfra"],
                     buttons=(
-                        MenuItemButton(
+                        PluginMenuButton(
                             link=f"plugins:sop_infra:class_add",
                             title="Add",
                             icon_class="mdi mdi-plus-thick",
@@ -24,18 +73,18 @@ INFRA = Menu(
                         ),
                     ),
                 ),
-                MenuItem(
+                PluginMenuItem(
                     link=f"plugins:sop_infra:sizing_list",
                     link_text=_("Sizings"),
                     permissions=[f"sop_infra.view_sopinfra"],
                     buttons=(
-                        MenuItemButton(
+                        PluginMenuButton(
                             link=f"plugins:sop_infra:sopinfra_refresh",
                             title="Recompute",
                             icon_class="mdi mdi-refresh",
                             permissions=[f"sop_infra.change_sop_infra"],
                         ),
-                        MenuItemButton(
+                        PluginMenuButton(
                             link=f"plugins:sop_infra:sizing_add",
                             title="Add",
                             icon_class="mdi mdi-plus-thick",
@@ -43,12 +92,12 @@ INFRA = Menu(
                         ),
                     ),
                 ),
-                MenuItem(
+                PluginMenuItem(
                     link=f"plugins:sop_infra:meraki_list",
                     link_text=_("Meraki SDWAN"),
                     permissions=[f"sop_infra.view_sopinfra"],
                     buttons=(
-                        MenuItemButton(
+                        PluginMenuButton(
                             link=f"plugins:sop_infra:meraki_add",
                             title="Add",
                             icon_class="mdi mdi-plus-thick",
@@ -58,15 +107,15 @@ INFRA = Menu(
                 ),
             ),
         ),
-        MenuGroup(
-            label=_("PRISMA"),
-            items=(
-                MenuItem(
+        (
+            _("PRISMA"),
+            (
+                PluginMenuItem(
                     link=f"plugins:sop_infra:prismaendpoint_list",
                     link_text=_("Endpoints"),
                     permissions=["sop_infra.view_prismaendpoint"],
                     buttons=(
-                        MenuItemButton(
+                        PluginMenuButton(
                             link=f"plugins:sop_infra:prismaendpoint_add",
                             title="Add",
                             icon_class="mdi mdi-plus-thick",
@@ -74,12 +123,12 @@ INFRA = Menu(
                         ),
                     ),
                 ),
-                MenuItem(
+                PluginMenuItem(
                     link=f"plugins:sop_infra:prismaaccesslocation_list",
                     link_text=_("Access Locations"),
                     permissions=["sop_infra.view_prismaaccesslocation"],
                     buttons=(
-                        MenuItemButton(
+                        PluginMenuButton(
                             link=f"plugins:sop_infra:prismaaccesslocation_add",
                             title="Add",
                             icon_class="mdi mdi-plus-thick",
@@ -87,12 +136,12 @@ INFRA = Menu(
                         ),
                     ),
                 ),
-                MenuItem(
+                PluginMenuItem(
                     link=f"plugins:sop_infra:prismacomputedaccesslocation_list",
                     link_text=_("Computed Access Locations"),
                     permissions=["sop_infra.view_computedaccesslocation"],
                     buttons=(
-                        MenuItemButton(
+                        PluginMenuButton(
                             link=f"plugins:sop_infra:prismacomputedaccesslocation_add",
                             title="Add",
                             icon_class="mdi mdi-plus-thick",
@@ -105,5 +154,3 @@ INFRA = Menu(
     ),
 )
 
-
-MENUS.append(INFRA)
