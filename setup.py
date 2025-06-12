c6b3e2641e651dfe2f5338e3e7fe00e4d66b43e8
tibobo67 <arnaud.brand--github@tib.cc>
2025-06-12 23:44:54 +0200

    prepare version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sop-infra",
-    version="0.4.17",
+    version="0.4.19",
     packages=find_packages(),
     include_package_data=True,
     description="Manage infrastructure informations of each site.",
