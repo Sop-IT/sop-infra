cc9cde2b271c5ec592b7a55aa360514933b62786
tibobo67 <arnaud.brand--github@tib.cc>
2025-06-19 10:02:56 +0200

    version bump

--- sop_infra/__init__.py
+++ sop_infra/__init__.py
@@ -5,11 +5,11 @@ class SopInfraConfig(PluginConfig):
     name = "sop_infra"
     verbose_name = "SOP Infra"
     description = "Manage infrastructure informations of each site"
-    version = "0.4.22"
+    version = "0.4.23"
     author = "Soprema NOC team"
     author_email = "noc@soprema.com"
     base_url = "sop-infra"
-    min_version = "4.2.0"
+    min_version = "4.3.0"
 
     def ready(self):
         super().ready()
