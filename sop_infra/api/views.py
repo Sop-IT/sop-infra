af364629b25e870c5fa0cec1ad805a83d324c86f
tibobo67 <arnaud.brand--github@tib.cc>
2025-07-09 10:55:22 +0200

    add fitlersets for API

--- sop_infra/api/views.py
+++ sop_infra/api/views.py
@@ -4,7 +4,7 @@ from netbox.api.viewsets import NetBoxModelViewSet
 from netbox.api.metadata import ContentTypeMetadata
 
 from sop_infra.models import *
-from sop_infra.filtersets import SopInfraFilterset, PrismaEndpointFilterset
+from sop_infra.filtersets import SopInfraFilterset, PrismaEndpointFilterset, SopMerakiDashFilterSet, SopMerakiDeviceFilterSet, SopMerakiNetFilterSet, SopMerakiOrgFilterSet
 from sop_infra.api.serializers import *
 
 
@@ -53,20 +53,23 @@ class SopMerakiDashViewSet(NetBoxModelViewSet):
         orgs_count=Count('orgs')
     )
     serializer_class = SopMerakiDashSerializer
-
+    filterset_class = SopMerakiDashFilterSet
 
 class SopMerakiOrgViewSet(NetBoxModelViewSet):
     queryset = SopMerakiOrg.objects\
         .annotate(nets_count=Count('nets', distinct=True))\
         .annotate(devices_count=Count('devices', distinct=True))
     serializer_class = SopMerakiOrgSerializer
+    filterset_class = SopMerakiOrgFilterSet
 
 
 class SopMerakiNetViewSet(NetBoxModelViewSet):
     queryset = SopMerakiNet.objects.all()
     serializer_class = SopMerakiNetSerializer
+    filterset_class = SopMerakiNetFilterSet
 
 
 class SopMerakiDeviceViewSet(NetBoxModelViewSet):
     queryset = SopMerakiDevice.objects.all()
     serializer_class = SopMerakiDeviceSerializer
+    filterset_class = SopMerakiDeviceFilterSet
