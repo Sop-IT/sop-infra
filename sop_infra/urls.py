0269a0c5fe0a9384f72b0adbc2cff393f0127998
tibobo67 <arnaud.brand--github@tib.cc>
2025-07-10 16:47:11 +0200

    remove useless views

--- sop_infra/urls.py
+++ sop_infra/urls.py
@@ -21,12 +21,11 @@ urlpatterns = [
     path('jsonexports/adsites', SopInfraJsonExportsAdSites.as_view(), name='jsonexports_adsites'),
 
     path('<int:pk>/', SopInfraDetailView.as_view(), name='sopinfra_detail'),
-    path('add/', SopInfraAddView.as_view(), name='sopinfra_add'),
-    path('add/<int:pk>/', SopInfraAddView.as_view(), name='sopinfra_add'),
+    # path('add/', SopInfraAddView.as_view(), name='sopinfra_add'),
+    # path('add/<int:pk>/', SopInfraAddView.as_view(), name='sopinfra_add'),
     path('edit/<int:pk>/', SopInfraEditView.as_view(), name='sopinfra_edit'),
     path('delete/<int:pk>/', SopInfraDeleteView.as_view(), name='sopinfra_delete'),
     path('refresh/', SopInfraRefreshView.as_view(), name='sopinfra_refresh'),
-    path('refresh_site/', SopInfraRefreshNoForm.as_view(), name='sopinfra_refresh_site'),
     path('journal/<int:pk>', ObjectJournalView.as_view(), name='sopinfra_journal', kwargs={'model': SopInfra}),
     path('changelog/<int:pk>', ObjectChangeLogView.as_view(), name='sopinfra_changelog', kwargs={'model': SopInfra}),
 
