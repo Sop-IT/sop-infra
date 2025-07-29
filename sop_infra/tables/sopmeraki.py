5df856b04a974de6dee97caef4aade8b5aabfa06
tibobo67 <arnaud.brand--github@tib.cc>
2025-07-29 17:02:26 +0200

    reformat

--- sop_infra/tables/sopmeraki.py
+++ sop_infra/tables/sopmeraki.py
@@ -1,15 +1,21 @@
 import django_tables2 as tables
 
 from netbox.tables import NetBoxTable, columns
-from sop_infra.models import SopMerakiOrg, SopMerakiDash, SopMerakiNet, SopMerakiDevice, SopMerakiSwitchStack
+from sop_infra.models import (
+    SopMerakiOrg,
+    SopMerakiDash,
+    SopMerakiNet,
+    SopMerakiDevice,
+    SopMerakiSwitchStack,
+)
 
 
 __all__ = (
-    'SopMerakiDashTable',
-    'SopMerakiOrgTable',
-    'SopMerakiNetTable',
-    'SopMerakiSwitchStackTable', 
-    'SopMerakiDeviceTable',
+    "SopMerakiDashTable",
+    "SopMerakiOrgTable",
+    "SopMerakiNetTable",
+    "SopMerakiSwitchStackTable",
+    "SopMerakiDeviceTable",
 )
 
 
@@ -20,8 +26,8 @@ class SopMerakiDashTable(NetBoxTable):
 
     class Meta(NetBoxTable.Meta):
         model = SopMerakiDash
-        fields = ('pk', 'id', 'nom', 'api_url', 'description', 'orgs_count')
-        default_columns = ('nom', 'description', 'api_url', 'orgs_count')
+        fields = ("pk", "id", "nom", "api_url", "description", "orgs_count")
+        default_columns = ("nom", "description", "api_url", "orgs_count")
 
 
 class SopMerakiOrgTable(NetBoxTable):
@@ -29,20 +35,37 @@ class SopMerakiOrgTable(NetBoxTable):
     nom = tables.Column(linkify=True)
     dash = tables.Column(linkify=True)
     nets_count = columns.LinkedCountColumn(
-        viewname='plugins:sop_infra:sopmerakinet_list',
-        url_params={'org_id': 'pk'},
-        verbose_name='Nets count'
+        viewname="plugins:sop_infra:sopmerakinet_list",
+        url_params={"org_id": "pk"},
+        verbose_name="Nets count",
     )
     devices_count = columns.LinkedCountColumn(
-        viewname='plugins:sop_infra:sopmerakidevice_list',
-        url_params={'org_id': 'pk'},
-        verbose_name='Devices count'
+        viewname="plugins:sop_infra:sopmerakidevice_list",
+        url_params={"org_id": "pk"},
+        verbose_name="Devices count",
     )
-    
+
     class Meta(NetBoxTable.Meta):
         model = SopMerakiOrg
-        fields = ('pk', 'id', 'nom', 'dash', 'description', 'meraki_id', 'meraki_url', 'nets_count', 'devices_count')
-        default_columns = ('dash', 'nom', 'description',  'meraki_url', 'nets_count', 'devices_count' )
+        fields = (
+            "pk",
+            "id",
+            "nom",
+            "dash",
+            "description",
+            "meraki_id",
+            "meraki_url",
+            "nets_count",
+            "devices_count",
+        )
+        default_columns = (
+            "dash",
+            "nom",
+            "description",
+            "meraki_url",
+            "nets_count",
+            "devices_count",
+        )
 
 
 class SopMerakiNetTable(NetBoxTable):
@@ -50,23 +73,43 @@ class SopMerakiNetTable(NetBoxTable):
     nom = tables.Column(linkify=True)
     org = tables.Column(linkify=True)
     site = tables.Column(linkify=True)
-    bound_to_template=tables.Column(verbose_name="Bound")
-    
+    bound_to_template = tables.Column(verbose_name="Bound")
+
     class Meta(NetBoxTable.Meta):
         model = SopMerakiNet
-        fields = ('pk', 'id', 'nom', 'org', 'site',  'meraki_id', 'meraki_url', 'meraki_notes', 'bound_to_template', 'ptypes', 'meraki_tags', 'timezone')
-        default_columns = ('nom',  'meraki_url',  'site', 'org', 'bound_to_template', 'meraki_notes')
+        fields = (
+            "pk",
+            "id",
+            "nom",
+            "org",
+            "site",
+            "meraki_id",
+            "meraki_url",
+            "meraki_notes",
+            "bound_to_template",
+            "ptypes",
+            "meraki_tags",
+            "timezone",
+        )
+        default_columns = (
+            "nom",
+            "meraki_url",
+            "site",
+            "org",
+            "bound_to_template",
+            "meraki_notes",
+        )
 
 
 class SopMerakiSwitchStackTable(NetBoxTable):
 
     nom = tables.Column(linkify=True)
     net = tables.Column(linkify=True)
-    
+
     class Meta(NetBoxTable.Meta):
         model = SopMerakiSwitchStack
-        fields = ('pk', 'id', 'nom', 'net', 'meraki_id', 'serials', 'members')
-        default_columns = ('nom',  'net')
+        fields = ("pk", "id", "nom", "net", "meraki_id", "serials", "members")
+        default_columns = ("nom", "net")
 
 
 class SopMerakiDeviceTable(NetBoxTable):
@@ -74,16 +117,36 @@ class SopMerakiDeviceTable(NetBoxTable):
     nom = tables.Column(linkify=True)
     org = tables.Column(linkify=True)
     site = tables.Column(linkify=True)
-    bound_to_template=tables.Column(verbose_name="Bound")
-    
+    meraki_network = tables.Column(linkify=True)
+    netbox_device = tables.Column(linkify=True, verbose_name="Netbox Device")
+    bound_to_template = tables.Column(verbose_name="Bound")
+
     class Meta(NetBoxTable.Meta):
         model = SopMerakiDevice
-        fields = ('pk', 'id', 'nom', 'org', 'site',  'serial', \
-            'meraki_netid', 'meraki_notes', 'meraki_network', 'ptype', \
-            'meraki_tags', 'meraki_details', 'firmware', 'netbox_device', \
-            'netbox_dev_type', 'model', 'mac', )
-        default_columns = ('nom',  'serial',  'site', 'org', 'meraki_network', 'meraki_notes')
-
-
-
-
+        fields = (
+            "pk",
+            "id",
+            "nom",
+            "org",
+            "site",
+            "serial",
+            "meraki_netid",
+            "meraki_notes",
+            "meraki_network",
+            "ptype",
+            "meraki_tags",
+            "meraki_details",
+            "firmware",
+            "netbox_device",
+            "netbox_dev_type",
+            "model",
+            "mac",
+        )
+        default_columns = (
+            "nom",
+            "serial",
+            "site",
+            "org",
+            "meraki_network",
+            "netbox_device",
+        )
