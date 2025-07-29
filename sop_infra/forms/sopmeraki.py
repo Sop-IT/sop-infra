82928d87ca8d18d686c6803493e0729860ea2d36
tibobo67 <arnaud.brand--github@tib.cc>
2025-07-29 17:02:55 +0200

    reformat

--- sop_infra/forms/sopmeraki.py
+++ sop_infra/forms/sopmeraki.py
@@ -5,129 +5,97 @@ from utilities.forms.fields import DynamicModelChoiceField
 from netbox.forms import NetBoxModelForm, NetBoxModelFilterSetForm
 from netbox.context import current_request
 from dcim.models import Site
-from sop_infra.models import SopMerakiDash, SopMerakiOrg, SopMerakiNet, SopMerakiDevice, SopMerakiSwitchStack
+from sop_infra.models import (
+    SopMerakiDash,
+    SopMerakiOrg,
+    SopMerakiNet,
+    SopMerakiDevice,
+    SopMerakiSwitchStack,
+)
 
 
 class SopMerakiDashForm(NetBoxModelForm):
 
     class Meta:
         model = SopMerakiDash
-        fields = ('nom', 'description', 'api_url')
+        fields = ("nom", "description", "api_url")
 
 
 class SopMerakiOrgForm(NetBoxModelForm):
 
     class Meta:
         model = SopMerakiOrg
-        fields = ('nom', 'dash', 'meraki_id', 'meraki_url')
+        fields = ("nom", "dash", "meraki_id", "meraki_url")
 
 
 class SopMerakiNetForm(NetBoxModelForm):
 
     class Meta:
         model = SopMerakiNet
-        fields = ('nom', 'site', 'org', 'meraki_id', 'bound_to_template', 'meraki_url')
+        fields = ("nom", "site", "org", "meraki_id", "bound_to_template", "meraki_url")
 
 
 class SopMerakiSwitchStackForm(NetBoxModelForm):
 
     class Meta:
         model = SopMerakiSwitchStack
-        fields = ('nom', 'net', 'meraki_id',)
+        fields = (
+            "nom",
+            "net",
+            "meraki_id",
+        )
 
 
 class SopMerakiDeviceForm(NetBoxModelForm):
 
     class Meta:
         model = SopMerakiDevice
-        fields = ('nom', 'serial', 'site', 'org', 'meraki_network')
+        fields = ("nom", "serial", "site", "org", "meraki_network")
 
 
 ##############  FILTER FORMS ################################
 
 
-
 class SopMerakiDashFilterForm(NetBoxModelFilterSetForm):
-    model=SopMerakiDash
-    nom=forms.CharField(
-        required=False
-    )
-    description=forms.CharField(
-        required=False
-    )
-    api_url=forms.CharField(
-        required=False
-    )
+    model = SopMerakiDash
+    nom = forms.CharField(required=False)
+    description = forms.CharField(required=False)
+    api_url = forms.CharField(required=False)
 
 
 class SopMerakiOrgFilterForm(NetBoxModelFilterSetForm):
-    model=SopMerakiOrg
-    nom=forms.CharField(
-        required=False
-    )
-    dash=DynamicModelChoiceField(
-        queryset=SopMerakiDash.objects.all(),
-        required=False
-    )
-    description=forms.CharField(
-        required=False
-    )
-    meraki_id=forms.CharField(
-        required=False
-    )
-    meraki_url=forms.CharField(
-        required=False
-    )
+    model = SopMerakiOrg
+    nom = forms.CharField(required=False)
+    dash = DynamicModelChoiceField(queryset=SopMerakiDash.objects.all(), required=False)
+    description = forms.CharField(required=False)
+    meraki_id = forms.CharField(required=False)
+    meraki_url = forms.CharField(required=False)
 
 
 class SopMerakiNetFilterForm(NetBoxModelFilterSetForm):
-    model=SopMerakiNet
-    nom=forms.CharField(
-        required=False
-    )
-    site=DynamicModelChoiceField(
-        queryset=Site.objects.all(),
-        required=False
-    )
-    org=DynamicModelChoiceField(
-        queryset=SopMerakiOrg.objects.all(),
-        required=False
-    )
-    bound_to_template=forms.BooleanField(
-        required=False
-    )
-    meraki_url=forms.CharField(
-        required=False
-    )
-    meraki_notes=forms.CharField(
-        required=False
-    )
-    meraki_id=forms.CharField(
-        required=False
-    )
+    model = SopMerakiNet
+    nom = forms.CharField(required=False)
+    site = DynamicModelChoiceField(queryset=Site.objects.all(), required=False)
+    org = DynamicModelChoiceField(queryset=SopMerakiOrg.objects.all(), required=False)
+    bound_to_template = forms.BooleanField(required=False)
+    meraki_url = forms.CharField(required=False)
+    meraki_notes = forms.CharField(required=False)
+    meraki_id = forms.CharField(required=False)
 
 
 class SopMerakiSwitchStackFilterForm(NetBoxModelFilterSetForm):
-    model=SopMerakiSwitchStack
-    nom=forms.CharField(
-        required=False
-    )
-    net=DynamicModelChoiceField(
-        queryset=SopMerakiNet.objects.all(),
-        required=False
-    )
-    meraki_id=forms.CharField(
-        required=False
-    )
+    model = SopMerakiSwitchStack
+    nom = forms.CharField(required=False)
+    net = DynamicModelChoiceField(queryset=SopMerakiNet.objects.all(), required=False)
+    meraki_id = forms.CharField(required=False)
 
 
 class SopMerakiDeviceFilterForm(NetBoxModelFilterSetForm):
-    model=SopMerakiDevice
-    nom=forms.CharField(
-        required=False
+    model = SopMerakiDevice
+    nom = forms.CharField(required=False)
+    stack = DynamicModelChoiceField(
+        queryset=SopMerakiSwitchStack.objects.all(), required=False
     )
-
-
 
 
 ##############  ACTION FORMS ################################
@@ -142,7 +110,7 @@ class SopMerakiDashRefreshForm(forms.Form):
         data = super().clean()
         dashs = SopMerakiDash.objects.none()
         base_url = reverse("plugins:sop_infra:sopmerakidash_list")
-        request:HttpRequest = current_request.get() # type: ignore
+        request: HttpRequest = current_request.get()  # type: ignore
 
         def normalize_queryset(obj):
             qs = [str(item) for item in obj]
@@ -152,19 +120,22 @@ class SopMerakiDashRefreshForm(forms.Form):
 
         if data["dash"]:
             dashs = SopMerakiDash.objects.filter(pk=data["dash"].pk)
-        else: 
+        else:
             dashs = SopMerakiDash.objects.all()
 
-        return_url=f"{base_url}?{normalize_queryset(dashs.values_list('id', flat=True))}"
+        return_url = (
+            f"{base_url}?{normalize_queryset(dashs.values_list('id', flat=True))}"
+        )
         if request.GET.get("return_url"):
-            return_url=request.GET.get("return_url")
+            return_url = request.GET.get("return_url")
 
-        details:bool=False
+        details: bool = False
         if data["details"]:
-            details=data["details"]
+            details = data["details"]
 
         return {
-            "dashs": dashs, "details":details,
+            "dashs": dashs,
+            "details": details,
             "return_url": return_url,
         }
 
@@ -178,7 +149,7 @@ class SopMerakiOrgRefreshChooseForm(forms.Form):
         data = super().clean()
         orgs = SopMerakiOrg.objects.none()
         base_url = reverse("plugins:sop_infra:sopmerakiorg_list")
-        request:HttpRequest = current_request.get() # type: ignore
+        request: HttpRequest = current_request.get()  # type: ignore
 
         def normalize_queryset(obj):
             qs = [str(item) for item in obj]
@@ -188,19 +159,22 @@ class SopMerakiOrgRefreshChooseForm(forms.Form):
 
         if data["org"]:
             orgs = SopMerakiOrg.objects.filter(pk=data["org"].pk)
-        else: 
+        else:
             orgs = SopMerakiOrg.objects.all()
 
-        return_url=f"{base_url}?{normalize_queryset(orgs.values_list('id', flat=True))}"
+        return_url = (
+            f"{base_url}?{normalize_queryset(orgs.values_list('id', flat=True))}"
+        )
         if request.GET.get("return_url"):
-            return_url=request.GET.get("return_url")
+            return_url = request.GET.get("return_url")
 
-        details:bool=False
+        details: bool = False
         if data["details"]:
-            details=data["details"]
+            details = data["details"]
 
         return {
-            "orgs": orgs, "details":details,
+            "orgs": orgs,
+            "details": details,
             "return_url": return_url,
         }
 
@@ -214,7 +188,7 @@ class SopMerakiNetRefreshChooseForm(forms.Form):
         data = super().clean()
         nets = SopMerakiNet.objects.none()
         base_url = reverse("plugins:sop_infra:sopmerakinet_list")
-        request:HttpRequest = current_request.get() # type: ignore
+        request: HttpRequest = current_request.get()  # type: ignore
 
         def normalize_queryset(obj):
             qs = [str(item) for item in obj]
@@ -223,20 +197,22 @@ class SopMerakiNetRefreshChooseForm(forms.Form):
             return f"id=" + "&id=".join(qs)
 
         if data["net"]:
-            orgs = SopMerakiNet.objects.filter(pk=data["net"].pk)
-        else: 
-            orgs = SopMerakiNet.objects.all()
+            nets = SopMerakiNet.objects.filter(pk=data["net"].pk)
+        else:
+            nets = SopMerakiNet.objects.all()
 
-        return_url=f"{base_url}?{normalize_queryset(orgs.values_list('id', flat=True))}"
+        return_url = (
+            f"{base_url}?{normalize_queryset(nets.values_list('id', flat=True))}"
+        )
         if request.GET.get("return_url"):
-            return_url=request.GET.get("return_url")
+            return_url = request.GET.get("return_url")
 
-        details:bool=False
+        details: bool = False
         if data["details"]:
-            details=data["details"]
+            details = data["details"]
 
         return {
-            "nets": nets, "details":details,
+            "nets": nets,
+            "details": details,
             "return_url": return_url,
         }
-
