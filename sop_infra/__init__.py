f36f1ab8764761989ef1ca9dad56c8800aa284c9
tibobo67 <arnaud.brand--github@tib.cc>
2025-08-01 14:54:19 +0200

    v0.4.40

--- sop_infra/__init__.py
+++ sop_infra/__init__.py
@@ -5,7 +5,7 @@ class SopInfraConfig(PluginConfig):
     name = "sop_infra"
     verbose_name = "SOP Infra"
     description = "Manage infrastructure informations of each site"
-    version = "0.4.39"
+    version = "0.4.40"
     author = "Soprema NOC team"
     author_email = "noc@soprema.com"
     base_url = "sop-infra"
