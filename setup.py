5389087dd26e5c416463c4600552275ced5bfd62
tibobo67 <arnaud.brand--github@tib.cc>
2025-09-01 17:03:11 +0200

    Fix API ordering + bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sop-infra",
-    version="0.4.47",
+    version="0.4.48",
     packages=find_packages(),
     include_package_data=True,
     description="Manage infrastructure informations of each site.",
--- sop_infra/api/views.py
+++ sop_infra/api/views.py
@@ -26,26 +26,26 @@ __all__ = (
 
 class PrismaEndpointViewSet(NetBoxModelViewSet):
     metadata_class = ContentTypeMetadata
-    queryset = PrismaEndpoint.objects.all()
+    queryset = PrismaEndpoint.objects.all().order_by("pk")
     serializer_class = PrismaEndpointSerializer
     filterset_class = PrismaEndpointFilterset
 
 
 class PrismaAccessLocationViewSet(NetBoxModelViewSet):
     metadata_class = ContentTypeMetadata
-    queryset = PrismaAccessLocation.objects.all()
+    queryset = PrismaAccessLocation.objects.all().order_by("pk")
     serializer_class = PrismaAccessLocationSerializer
 
 
 class PrismaComputedAccessLocationViewSet(NetBoxModelViewSet):
     metadata_class = ContentTypeMetadata
-    queryset = PrismaComputedAccessLocation.objects.all()
+    queryset = PrismaComputedAccessLocation.objects.all().order_by("pk")
     serializer_class = PrismaComputedAccessLocationSerializer
 
 
 class SopInfraViewSet(NetBoxModelViewSet):
     metadata_class = ContentTypeMetadata
-    queryset = SopInfra.objects.all()
+    queryset = SopInfra.objects.all().order_by("pk")
     serializer_class = SopInfraSerializer
     filterset_class = SopInfraFilterset
 
@@ -54,7 +54,7 @@ class SopMerakiDashViewSet(NetBoxModelViewSet):
     queryset = SopMerakiDash.objects.prefetch_related("tags")\
         .annotate(orgs_count=Count("orgs"))\
         .annotate(nets_count=Count("orgs__nets", distinct=True))\
-        .annotate(devs_count=Count("orgs__devices", distinct=True))
+        .annotate(devs_count=Count("orgs__devices", distinct=True)).order_by("pk")
     serializer_class = SopMerakiDashSerializer
     filterset_class = SopMerakiDashFilterSet
 
@@ -62,37 +62,37 @@ class SopMerakiDashViewSet(NetBoxModelViewSet):
 class SopMerakiOrgViewSet(NetBoxModelViewSet):
     queryset = SopMerakiOrg.objects\
         .annotate(nets_count=Count("nets", distinct=True))\
-        .annotate(devs_count=Count("devices", distinct=True))
+        .annotate(devs_count=Count("devices", distinct=True)).order_by("pk")
     serializer_class = SopMerakiOrgSerializer
     filterset_class = SopMerakiOrgFilterSet
 
 
 class SopMerakiNetViewSet(NetBoxModelViewSet):
     queryset = SopMerakiNet.objects.all()\
-        .annotate(devs_count=Count("devices", distinct=True))
+        .annotate(devs_count=Count("devices", distinct=True)).order_by("pk")
     serializer_class = SopMerakiNetSerializer
     filterset_class = SopMerakiNetFilterSet
 
 
 class SopMerakiSwitchStackViewSet(NetBoxModelViewSet):
-    queryset = SopMerakiSwitchStack.objects.all()
+    queryset = SopMerakiSwitchStack.objects.all().order_by("pk")
     serializer_class = SopMerakiSwitchStackSerializer
     filterset_class = SopMerakiSwitchStackFilterSet
 
 
 class SopMerakiDeviceViewSet(NetBoxModelViewSet):
-    queryset = SopMerakiDevice.objects.all()
+    queryset = SopMerakiDevice.objects.all().order_by("pk")
     serializer_class = SopMerakiDeviceSerializer
     filterset_class = SopMerakiDeviceFilterSet
 
 
 class SopSwitchTemplateViewSet(NetBoxModelViewSet):
-    queryset = SopSwitchTemplate.objects.all()
+    queryset = SopSwitchTemplate.objects.all().order_by("pk")
     serializer_class = SopSwitchTemplateSerializer
     filterset_class = SopSwitchTemplateFilterset
 
 
 class SopDeviceSettingViewSet(NetBoxModelViewSet):
-    queryset = SopDeviceSetting.objects.all()
+    queryset = SopDeviceSetting.objects.all().order_by("pk")
     serializer_class = SopDeviceSettingSerializer
     filterset_class = SopDeviceSettingFilterset
