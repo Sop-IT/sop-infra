5f1cead452611043c410d9a90ceece02d8bcfc4d
tibobo67 <arnaud.brand--github@tib.cc>
2026-01-28 16:22:25 +0100

    bump version to 0.4.56

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sop-infra",
-    version="0.4.55",
+    version="0.4.56",
     packages=find_packages(),
     include_package_data=True,
     description="Manage infrastructure informations of each site.",
--- sop_infra/__init__.py
+++ sop_infra/__init__.py
@@ -5,7 +5,7 @@ class SopInfraConfig(PluginConfig):
     name = "sop_infra"
     verbose_name = "SOP Infra"
     description = "Manage infrastructure informations of each site"
-    version = "0.4.55"
+    version = "0.4.56"
     author = "Soprema NOC team"
     author_email = "noc@soprema.com"
     base_url = "sop-infra"
