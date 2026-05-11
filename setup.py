96cbd5468d380faa902e941d26f6c0905960e64d
tibobo67 <arnaud.brand--github@tib.cc>
2026-05-11 14:22:51 +0200

    version bump to 0.4.74

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sop-infra",
-    version="0.4.73",
+    version="0.4.74",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
