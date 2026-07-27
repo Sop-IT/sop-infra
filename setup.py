ccc8940ddf93dac1bd85d58ff98789b536f11c91
tibobo67 <arnaud.brand--github@tib.cc>
2026-07-27 09:38:56 +0200

    Bump to 0.5.14 to merge VPN exclusions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sop-infra",
-    version="0.5.13",
+    version="0.5.14",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
