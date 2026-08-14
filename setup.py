011a510b0928e404d49975103a7c057b5069b8cf
tibobo67 <arnaud.brand--github@tib.cc>
2026-08-14 14:17:24 +0200

    v0.5.20 - better claim, better move, better filtering

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sop-infra",
-    version="0.5.19",
+    version="0.5.20",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
