adeb409e09cb56f78bd17d12b291e7993d10792a
tibobo67 <arnaud.brand--github@tib.cc>
2026-08-25 10:22:58 +0200

    bump to 0.5.24 to fix packaging issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sop-infra",
-    version="0.5.23",
+    version="0.5.24",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
